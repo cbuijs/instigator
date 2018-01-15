1935a93b910fd18aa99ad59632557aeefe8e63cc
Chris Buijs <cbuijs@chrisbuijs.com>
2018-01-15 18:19:00 -0500

    Cleanup

--- instigator.py
+++ instigator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 '''
 =========================================================================================
- instigator.py: v0.2-20180115 Copyright (C) 2017 Chris Buijs <cbuijs@chrisbuijs.com>
+ instigator.py: v0.3-20180115 Copyright (C) 2017 Chris Buijs <cbuijs@chrisbuijs.com>
 =========================================================================================
 
  Python DNS Server with security and filtering features
@@ -12,7 +12,7 @@
 '''
 
 # Standard modules
-import sys
+import sys, time
 
 # make sure modules can be found
 sys.path.append("/usr/local/lib/python2.7/dist-packages/")
@@ -28,44 +28,22 @@ blacklist['doubleclick.net'] = True
 blacklist['google-analytics.com'] = True
 blacklist['google-analytics.com'] = True
 
-# Timeout on forwarding
-timeout = 20
-
-# Listen on
-listen = '127.0.0.1'
+# Listen for queries
+listen_address = '127.0.0.1'
 listen_port = 5053
 
 # Forwarding queries to
-forward_dns = '9.9.9.9' # Quad9
+forward_address = '9.9.9.9' # Quad9
 forward_port = 53
+forward_timeout = 20 # Seconds
 
 # Redirect when blacklisted, leave empty for "Refused"
-#redirect_address = '192.168.1.250'
-redirect_address = ''
-redirect_host = 'dummy'
+redirect_address = '192.168.1.250'
+#redirect_address = ''
+redirect_host = 'sinkhole' # For CNAME, MX, PTR, SRV
 
-class SubProxy(ProxyResolver):
-    def __init__(self, address, port, timeout, answer, host):
-        ProxyResolver.__init__(self, address, port, timeout)
-        self.answer = answer
-        self.host = host
 
-    def resolve(self, request, handler):
-        if in_list('black', request.q.qname):
-            reply = request.reply()
-
-            if len(self.host) == 0:
-                reply.header.rcode = getattr(RCODE,'REFUSED')
-            else:
-                answer = RR(rdata=A(self.host))
-                answer.set_rname(request.q.qname)
-                reply.add_answer(answer)
-                reply.header.rcode = getattr(RCODE,'NOERROR')
-
-            return reply
-        else:
-            return ProxyResolver.resolve(self, request, handler)
-
+# Check if entry matches a list
 def in_list(type, name):
     name = str(name).rstrip('.').lower()
     testname = name
@@ -87,14 +65,42 @@ def in_list(type, name):
         return False
        
 
+class DNS_Instigator(ProxyResolver):
+    def __init__(self, forward_address, forward_port, forward_timeout, redirect_host, redirect_address):
+        ProxyResolver.__init__(self, forward_address, forward_port, forward_timeout)
+        self.redirect_host = redirect_host
+        self.redirect_address = redirect_address
+
+    def resolve(self, request, handler):
+        if in_list('black', request.q.qname):
+            reply = request.reply()
+
+            if len(self.redirect_address) == 0:
+                reply.header.rcode = getattr(RCODE,'REFUSED')
+            else:
+                answer = RR(ttl=1800, rdata=A(self.redirect_address))
+                answer.set_rname(request.q.qname)
+                reply.add_answer(answer)
+                reply.header.rcode = getattr(RCODE,'NOERROR')
+
+            return reply
+        else:
+            return ProxyResolver.resolve(self, request, handler)
+
+
+# The main beef
 if __name__ == '__main__':
-    import time
 
-    resolver = SubProxy(address=forward_dns, port=forward_port, timeout=timeout, answer=redirect_host, host=redirect_address) 
+    # Resolver
+    dns_resolver = DNS_Instigator(forward_address=forward_address, forward_port=forward_port, forward_timeout=forward_timeout, redirect_host=redirect_host, redirect_address=redirect_address) 
 
-    server = DNSServer(resolver, address=listen, port=listen_port)
-    
-    server.start_thread()
+    # Server
+    dns_server = DNSServer(dns_resolver, address=listen_address, port=listen_port)
 
-    while server.isAlive():
+    # Start server
+    dns_server.start_thread()
+
+    # Keep things running
+    while dns_server.isAlive():
         time.sleep(1)
+
