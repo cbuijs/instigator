b3b827a08872ed8cc4299274df5f198a0f801f7d
Chris Buijs <cbuijs@chrisbuijs.com>
2018-04-24 17:29:03 -0400

    Small fixes/optmizations

--- instigator.py
+++ instigator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 '''
 =========================================================================================
- instigator.py: v0.80-20180424 Copyright (C) 2018 Chris Buijs <cbuijs@chrisbuijs.com>
+ instigator.py: v0.83-20180424 Copyright (C) 2018 Chris Buijs <cbuijs@chrisbuijs.com>
 =========================================================================================
 
 Python DNS Server with security and filtering features
@@ -77,8 +77,8 @@ wl_rx = dict() # Regex Whitelist
 bl_rx = dict() # Regex Blacklist
 
 # Cache Dictionaries
-wl_cache = TTLCache(cachesize, cachettl) # Whitelist hit cache
-bl_cache = TTLCache(cachesize, cachettl) # Blacklist hit cache
+wl_cache = TTLCache(cachesize, cachettl - 1) # Whitelist hit cache
+bl_cache = TTLCache(cachesize, cachettl - 1) # Blacklist hit cache
 
 # Regex to filter IP's out
 ip4regex_text = '((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}(/(3[0-2]|[12]?[0-9]))*)'
@@ -96,79 +96,79 @@ isregex = regex.compile('^/.*/$')
 ##############################################################
 
 # Check if entry matches a list
-def in_blacklist(type, name):
-    testname = name
+def in_blacklist(type, value):
+    testvalue = value
 
-    if (testname in wl_cache):
-        print('WHITELIST-CACHE-HIT: ' + type + ' \"' + name + '\"')
+    if (testvalue in wl_cache):
+        print('WHITELIST-CACHE-HIT: ' + type + ' \"' + value + '\"')
         return False
-    elif isdomain.match(testname) and (testname in wl_dom):
-        wl_cache[name] = True
-        print('WHITELIST-HIT: ' + type + ' \"' + name + '\" matched against \"' + testname + '\"')
+    elif isdomain.match(testvalue) and (testvalue in wl_dom):
+        wl_cache[value] = True
+        print('WHITELIST-HIT: ' + type + ' \"' + value + '\" matched against \"' + testvalue + '\"')
         return False
 
-    if (testname in bl_cache):
-        print('BLACKLIST-CACHE-HIT: ' + type + ' \"' + name + '\"')
+    if (testvalue in bl_cache):
+        print('BLACKLIST-CACHE-HIT: ' + type + ' \"' + value + '\"')
         return True
-    elif isdomain.match(testname) and (testname in bl_dom):
-        bl_cache[name] = True
-        print('BLACKLIST-HIT: ' + type + ' \"' + name + '\" matched against \"' + testname + '\"')
+    elif isdomain.match(testvalue) and (testvalue in bl_dom):
+        bl_cache[value] = True
+        print('BLACKLIST-HIT: ' + type + ' \"' + value + '\" matched against \"' + testvalue + '\"')
         return True
 
-    if type == 'RESPONSE' and ipregex.match(testname):
+    if type == 'REPLY' and ipregex.match(testvalue):
         found = False
         prefix = False
-        if ipregex4.match(testname):
+        if ipregex4.match(testvalue):
             wip = wl_ip4
             bip = bl_ip4
         else:
             wip = wl_ip6
             bip = bl_ip6
 
-        if not testname in wip:
-            if testname in bip:
-                prefix = bip.get_key(testname)
+        if not testvalue in wip:
+            if testvalue in bip:
+                prefix = bip.get_key(testvalue)
                 found = True
         else:
-            prefix = wip.get_key(testname)
+            prefix = wip.get_key(testvalue)
 
         if found:
-            print('BLACKLIST-IP-HIT: ' + type + ' ' + name + ' matched against ' + prefix)
-            bl_cache[name] = True
+            print('BLACKLIST-IP-HIT: ' + type + ' ' + value + ' matched against ' + prefix)
+            bl_cache[value] = True
             return True
         elif prefix:
-            print('WHITELIST-IP-HIT: ' + type + ' ' + name + ' matched against ' + prefix)
+            print('WHITELIST-IP-HIT: ' + type + ' ' + value + ' matched against ' + prefix)
             return False
 
     else:
-        if testname.find('.') > 0:
-            testname = testname[testname.find('.') + 1:]
-            while testname:
-                if testname in wl_dom:
-                    wl_cache[name] = True
-                    print('WHITELIST-HIT: ' + type + ' \"' + name + '\" matched against \"' + testname + '\"')
+        if testvalue.find('.') > 0:
+            testvalue = testvalue[testvalue.find('.') + 1:]
+            while testvalue:
+                if testvalue in wl_dom:
+                    wl_cache[value] = True
+                    print('WHITELIST-HIT: ' + type + ' \"' + value + '\" matched against \"' + testvalue + '\"')
                     return False
-                if testname in bl_dom:
-                    bl_cache[name] = True
-                    print('BLACKLIST-HIT: ' + type + ' \"' + name + '\" matched against \"' + testname + '\"')
+                if testvalue in bl_dom:
+                    bl_cache[value] = True
+                    print('BLACKLIST-HIT: ' + type + ' \"' + value + '\" matched against \"' + testvalue + '\"')
                     return True
-                elif testname.find('.') == -1:
+                elif testvalue.find('.') == -1:
                     break
                 else:
-                    testname = testname[testname.find('.') + 1:]
+                    testvalue = testvalue[testvalue.find('.') + 1:]
 
     for i in wl_rx.keys():
         rx = wl_rx[i]
-        if rx.match(name):
-            print('WHITELIST-REGEX-HIT: ' + type + ' \"' + name + '\" matched against \"' + i + '\"')
-            wl_cache[name] = True
+        if rx.match(value):
+            print('WHITELIST-REGEX-HIT: ' + type + ' \"' + value + '\" matched against \"' + i + '\"')
+            wl_cache[value] = True
             return False
 
     for i in bl_rx.keys():
         rx = bl_rx[i]
-        if rx.match(name):
-            print('BLACKLIST-REGEX-HIT: ' + type + ' \"' + name + '\" matched against \"' + i + '\"')
-            bl_cache[name] = True
+        if rx.match(value):
+            print('BLACKLIST-REGEX-HIT: ' + type + ' \"' + value + '\" matched against \"' + i + '\"')
+            bl_cache[value] = True
             return True
 
     return False
@@ -183,14 +183,14 @@ def generate_response(request, qname, qtype, redirect_address):
         return reply
     else:
         print('REDIRECT \"' + qname + '\" to \"' + redirect_address + '\" (RR:' + qtype + ')')
-        answer = RR(ttl=cachettl, rdata=A(redirect_address))
+        #answer = RR(ttl=cachettl, rdata=A(redirect_address))
+        answer = RR(ttl=minttl, rdata=A(redirect_address))
 
     answer.set_rname(request.q.qname)
     reply.add_answer(answer)
     reply.header.rcode = getattr(RCODE,'NOERROR')
 
     return reply
-
 
 def read_list(file, listname, domlist, iplist4, iplist6, rxlist):
     print('Reading \"' + listname + '\" (' + file + ')')
@@ -222,8 +222,8 @@ def read_list(file, listname, domlist, iplist4, iplist6, rxlist):
     return True
 
 
-def normalize_ttl(reply):
-    ttl = min(x.ttl for x in reply)
+def normalize_ttl(rr):
+    ttl = min(x.ttl for x in rr)
     if ttl < minttl:
         ttl = minttl
     elif ttl > maxttl:
@@ -241,7 +241,8 @@ class DNS_Instigator(ProxyResolver):
         qtype = QTYPE[request.q.qtype]
         query = str(request.q)
 
-        if in_blacklist('QUERY', qname):
+        if in_blacklist('REQUEST', qname):
+            print('CHECK-REQUEST: ', qname, qtype)
             reply = generate_response(request, qname, qtype, redirect_address)
         else:
             reply = ProxyResolver.resolve(self, request, handler)
@@ -255,7 +256,8 @@ class DNS_Instigator(ProxyResolver):
                         rqname = str(record.rname).rstrip('.').lower()
                         rqtype = QTYPE[record.rtype]
                         data = str(record.rdata).rstrip('.').lower()
-                        if in_blacklist('QUERY', rqname) or in_blacklist('RESPONSE', data):
+                        print('CHECK-REPLY: ', rqname, rqtype, data)
+                        if in_blacklist('REQUEST', rqname) or in_blacklist('REPLY', data):
                             reply = generate_response(request, qname, qtype, redirect_address)
                             break
 
@@ -276,13 +278,13 @@ if __name__ == '__main__':
     dns_resolver = DNS_Instigator(forward_address=forward_address, forward_port=forward_port, forward_timeout=forward_timeout, redirect_host=redirect_host, redirect_address=redirect_address) 
 
     # Server
-    logger = DNSLogger(prefix=False)
+    logger = DNSLogger(log='request, reply, truncated, error', prefix=False)
     dns_server = DNSServer(dns_resolver, address=listen_address, port=listen_port, logger=logger)
 
     # Start server
     print('Starting DNS Service ...')
     dns_server.start_thread()
-    print('DNS Service ready!')
+    print('DNS Service ready on ' + listen_address + ':' + str(listen_port))
 
     # Keep things running
     count = 0
