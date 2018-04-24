3416783fc87b7a32ec0bd7e209fce97b3c2582fe
Chris Buijs <cbuijs@chrisbuijs.com>
2018-04-24 11:23:19 -0400

    Reading lists

--- instigator.py
+++ instigator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 '''
 =========================================================================================
- instigator.py: v0.65-20180424 Copyright (C) 2018 Chris Buijs <cbuijs@chrisbuijs.com>
+ instigator.py: v0.70-20180424 Copyright (C) 2018 Chris Buijs <cbuijs@chrisbuijs.com>
 =========================================================================================
 
 Python DNS Server with security and filtering features
@@ -30,6 +30,7 @@ sys.path.append("/usr/local/lib/python2.7/dist-packages/")
 from dnslib import RCODE, QTYPE, RR, A, AAAA, CNAME, MX, NS, PTR, SOA, SRV, TXT, RCODE, DNSRecord
 from dnslib.proxy import ProxyResolver
 from dnslib.server import DNSServer
+from dnslib.server import DNSLogger
 
 # Regex module
 import regex
@@ -39,7 +40,7 @@ import pytricia
 
 # Listen for queries
 listen_address = '192.168.1.250'
-listen_port = 53053
+listen_port = 53
 
 # Forwarding queries to
 forward_address = '1.1.1.1' # CloudFlare
@@ -47,13 +48,17 @@ forward_port = 53
 forward_timeout = 20 # Seconds
 
 # Redirect Address, leave empty to generete REFUSED
-redirect_address = ''
-#redirect_address = '192.168.1.250' # IPv4 only
+#redirect_address = ''
+redirect_address = '192.168.1.250' # IPv4 only
 redirect_host = 'sinkhole'
 
 # TTL settings
 minttl = 120
 maxttl = 3600
+
+# Files
+blacklist = '/opt/instigator/black.list'
+whitelist = '/opt/instigator/white.list'
 
 # Cache
 cachesize = 2500
@@ -75,14 +80,6 @@ wl_ip6 = pytricia.PyTricia(128)
 bl_cache = dict()
 wl_cache = dict()
 
-# Test
-wl_dom['chrisbuijs.com'] = True
-bl_dom['doubleclick.net'] = True
-bl_dom['google-analytics.com'] = True
-bl_dom['googleadapis.l.google.com'] = True
-bl_ip4['127.0.0.1'] = True # Test with "localhost"
-bl_ip4['194.109.6.0/25'] = True # Test with "www.xs4all.nl"
-
 # Regex to filter IP's out
 ip4regex_text = '((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}(/(3[0-2]|[12]?[0-9]))*)'
 ip6regex_text = '(((:(:[0-9a-f]{1,4}){1,7}|::|[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){1,6}|::|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){1,5}|::|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){1,4}|::|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){1,3}|::|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){1,2}|::|:[0-9a-f]{1,4}(::[0-9a-f]{1,4}|::|:[0-9a-f]{1,4}(::|:[0-9a-f]{1,4}))))))))|(:(:[0-9a-f]{1,4}){0,5}|[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){0,4}|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){0,3}|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4}){0,2}|:[0-9a-f]{1,4}(:(:[0-9a-f]{1,4})?|:[0-9a-f]{1,4}(:|:[0-9a-f]{1,4})))))):(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3})(/(12[0-8]|1[01][0-9]|[1-9]?[0-9]))*)'
@@ -92,22 +89,29 @@ ipregex = regex.compile('^(' + ip4regex_text + '|' + ip6regex_text +')$', regex.
 #ipregex = regex.compile('^(([0-9]{1,3}\.){3}[0-9]{1,3}(/[0-9]{1,2})*|([0-9a-f]{1,4}|:)(:([0-9a-f]{0,4})){1,7}(/[0-9]{1,3})*)$', regex.I)
 
 # Regex to match domains/hosts in lists
-#isdomain = regex.compile('^[a-z0-9\.\-]+$', regex.I) # According RFC, Internet only
+isdomain = regex.compile('^[a-z0-9\.\-]+$', regex.I) # According RFC, Internet only
 
 # Regex to filter regexes out
-#isregex = regex.compile('^/.*/$')
+isregex = regex.compile('^/.*/$')
 
 
 # Check if entry matches a list
 def in_blacklist(type, name):
     testname = name
 
-    if (testname in wl_cache) or (testname in wl_dom):
-        print ('WHITELIST-HIT: ' + type + ' \"' + name + '\" (Stage 1)')
+    if (testname in wl_cache):
+        print('WHITELIST-CACHE-HIT: ' + type + ' \"' + name + '\"')
         return False
+    elif (testname in wl_dom):
+        wl_cache[name] = True
+        print('WHITELIST-HIT: ' + type + ' \"' + name + '\" matched against \"' + testname + '\"')
 
-    if (testname in bl_cache) or (testname in bl_dom):
-        print ('BLACKLIST-HIT: ' + type + ' \"' + name + '\" (Stage 1)')
+    if (testname in bl_cache):
+        print('BLACKLIST-CACHE-HIT: ' + type + ' \"' + name + '\"')
+        return True
+    elif (testname in bl_dom):
+        bl_cache[name] = True
+        print('BLACKLIST-HIT: ' + type + ' \"' + name + '\" matched against \"' + testname + '\"')
         return True
 
     if type == 'RESPONSE' and ipregex.match(testname):
@@ -128,29 +132,31 @@ def in_blacklist(type, name):
             prefix = wip.get_key(testname)
 
         if found:
-            print ('BLACKLIST-IP-HIT: ' + type + ' ' + name + ' matched against ' + prefix)
+            print('BLACKLIST-IP-HIT: ' + type + ' ' + name + ' matched against ' + prefix)
             bl_cache[name] = True
             return True
         elif prefix:
-            print ('WHITELIST-IP-HIT: ' + type + ' ' + name + ' matched against ' + prefix)
+            print('WHITELIST-IP-HIT: ' + type + ' ' + name + ' matched against ' + prefix)
             return False
 
     else:
-        while True:
-            if testname in wl_dom:
-                wl_cache[name] = True
-                print ('WHITELIST-HIT: ' + type + ' \"' + name + '\" matched against \"' + testname + '\" (Stage 2)')
-                return False
-            if testname in bl_dom:
-                bl_cache[name] = True
-                print ('BLACKLIST-HIT: ' + type + ' \"' + name + '\" matched against \"' + testname + '\" (Stage 2)')
-                return False
-            elif testname.find('.') == -1:
-                break
-            else:
-                testname = testname[testname.find('.') + 1:]
+        if testname.find('.') > 0:
+            testname = testname[testname.find('.') + 1:]
+            while testname:
+                if testname in wl_dom:
+                    wl_cache[name] = True
+                    print('WHITELIST-HIT: ' + type + ' \"' + name + '\" matched against \"' + testname + '\"')
+                    return False
+                if testname in bl_dom:
+                    bl_cache[name] = True
+                    print('BLACKLIST-HIT: ' + type + ' \"' + name + '\" matched against \"' + testname + '\"')
+                    return True
+                elif testname.find('.') == -1:
+                    break
+                else:
+                    testname = testname[testname.find('.') + 1:]
 
-    # Do regex here
+    ### !!! Do regex here
 
     return False
        
@@ -159,11 +165,11 @@ def in_blacklist(type, name):
 def generate_response(request, qname, qtype, redirect_address):
     reply = request.reply()
     if (len(redirect_address) == 0) or (qtype not in ('A', 'CNAME', 'ANY')):
-        print ('REFUSED for \"' + qname + '\" (RR:' + qtype + ')')
+        print('REFUSED for \"' + qname + '\" (RR:' + qtype + ')')
         reply.header.rcode = getattr(RCODE,'REFUSED')
         return reply
     else:
-        print ('REDIRECT \"' + qname + '\" to \"' + redirect_address + '\" (RR:' + qtype + ')')
+        print('REDIRECT \"' + qname + '\" to \"' + redirect_address + '\" (RR:' + qtype + ')')
         answer = RR(ttl=cachettl, rdata=A(redirect_address))
 
     answer.set_rname(request.q.qname)
@@ -192,6 +198,32 @@ def update_cache(msg):
            del cacheindex[query]
 
     return True
+
+def read_list(file, listname, domlist, iplist4, iplist6):
+
+    print('Reading \"' + listname + '\" (' + file + ')')
+
+    count = 0
+    try:
+        with open(file, 'r') as f:
+            for line in f:
+                count += 1
+                entry = line.replace('\r', '').replace('\n', '').strip().lower()
+                if not (entry.startswith("#")) and not (len(entry) == 0):
+                    if ipregex4.match(entry):
+                        iplist4[entry] = True
+                    elif ipregex6.match(entry):
+                        iplist6[entry] = True
+                    else:
+                        domlist[entry] = True
+
+    except BaseException as err:
+             print ('ERROR: Unable to open/read/process file \"' + file + ' - ' + str(err))
+
+    print(listname + ': ' + str(len(iplist4)) + ' IPv4 CIDRs, ' + str(len(iplist6)) + ' IPv6 CIDRs and ' + str(len(domlist)) + ' DOMAINS')
+
+    return True
+
 
 class DNS_Instigator(ProxyResolver):
     def __init__(self, forward_address, forward_port, forward_timeout, redirect_host, redirect_address):
@@ -263,14 +295,21 @@ class DNS_Instigator(ProxyResolver):
 # The main beef
 if __name__ == '__main__':
 
+    # Read Lists
+    read_list(whitelist, 'Whitelist', wl_dom, wl_ip4, wl_ip6)
+    read_list(blacklist, 'Blacklist', bl_dom, bl_ip4, bl_ip6)
+
     # Resolver
     dns_resolver = DNS_Instigator(forward_address=forward_address, forward_port=forward_port, forward_timeout=forward_timeout, redirect_host=redirect_host, redirect_address=redirect_address) 
 
     # Server
-    dns_server = DNSServer(dns_resolver, address=listen_address, port=listen_port)
+    logger = DNSLogger(prefix=False)
+    dns_server = DNSServer(dns_resolver, address=listen_address, port=listen_port, logger=logger)
 
     # Start server
+    print('Starting DNS Service ...')
     dns_server.start_thread()
+    print('DNS Service ready!')
 
     # Keep things running
     count = 0
